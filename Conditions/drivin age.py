b280d4379fbf517e8e0058df903d16a90bf135c0
Sathya <hssathya@gmail.com>
2021-05-11 18:39:11 +0530

    changed file name

--- Conditions/drivin age.py
+++ Conditions/drivin age.py
@@ -1,5 +1,5 @@
 driving_age = eval(input("What is the legal driving age where you live? "))
-your_age = eval(input("How old are you? "))
+your_age = eval(input("How old are you?"))
 if your_age >= driving_age:
     print("You're old enough to drive!")
 if your_age < driving_age:
